8871687e4977d0a51069088156880188db574953
David Jeusette <jeusette.david@gmail.com>
2015-08-13 14:52:59 +0200

    Test secure token builder

--- spec/re_captcha/secure_token_builder_spec.rb
+++ spec/re_captcha/secure_token_builder_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe ReCaptcha::SecureTokenBuilder do
+  it { expect(described_class).to respond_to(:build) }
 
+  describe '.build' do
+    let(:key) { 'my secret key' }
+
+    it 'builds a secure token' do
+      expect(described_class.build(key)).to be_a(String)
+      expect(described_class.build(key)).not_to be_empty
+    end
+  end
 end
