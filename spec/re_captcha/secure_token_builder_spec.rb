5e83b41910e903838b05852932020c30358ba787
David Jeusette <jeusette.david@gmail.com>
2015-08-19 11:41:04 +0200

    Use Time.at in tests

--- spec/re_captcha/secure_token_builder_spec.rb
+++ spec/re_captcha/secure_token_builder_spec.rb
@@ -15,7 +15,7 @@ describe ReCaptcha::SecureTokenBuilder do
       end
 
       it 'builds a token with a correct value' do
-        time = Time.parse("Feb 24 1981")
+        time = Time.at(351817200)
         allow(Time).to receive(:now).and_return(time)
         allow(SecureRandom).to receive(:uuid).and_return('8a180d31-c031-4258-a36f-bc4207f67bef')
         expect(instance.build).to eq("95ZHArcXmvPlPBc6r95vSj_83vWuXuetY9KfVy4O6AszqvTE_ok6u85L74jMQYmYKtIEqjOJzUgiK9kSjEROQYHleYCsftaXldeLgxeQCFI")
