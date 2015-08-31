1098fb0a180a2999df6e9a62069faba34c62cd11
David Jeusette <jeusette.david@gmail.com>
2015-08-31 13:57:50 +0200

    Add resolution hints in the ConfigurationError

--- lib/re_captcha/exceptions.rb
+++ lib/re_captcha/exceptions.rb
@@ -1,3 +1,21 @@
 module ReCaptcha
-  class ConfigurationError < StandardError; end
+  class ConfigurationError < StandardError
+    def initialize(name)
+      super(compose_message(name))
+    end
+
+    private
+
+    def compose_message(name)
+      problem = name
+      resolution = ''
+      resolution << %(\nMake sure to have configured the gem using the configure block)
+      resolution << %(\nReCaptcha.configure do |config|)
+      resolution << %(\n\tconfig.private_key = "private_key")
+      resolution << %(\n\tconfig.public_key = "public_key")
+      resolution << %(\nend)
+
+      "#{problem} #{resolution}"
+    end
+  end
 end
